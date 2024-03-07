0546ae19443711af3f64749307ac11468b7c92fb
J. Graham <polymathy78@gmail..com>
2024-03-06 19:46:45 -0500

    update counter in db

--- themes/mytheme/static/js/main.js
+++ themes/mytheme/static/js/main.js
@@ -11,11 +11,29 @@ function getCounter() {
       const parsedData = JSON.parse(counter.body);
       let currentCount = parsedData.Item.counter.N;
       console.log('current count:', currentCount);
-
       elem.innerHTML = currentCount;
       console.log(elem);
+      currentCount++;
+      setCounter(currentCount);
     })
     .catch((error) => {
       elem.innerHTML = '#';
     });
+}
+
+function setCounter(num) {
+  console.log('running set');
+  fetch(
+    'https://zrzq671re4.execute-api.us-east-1.amazonaws.com/dev/set_counter',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        website: 'jeffgraham.cloud',
+        counter: num.toString(),
+      }),
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error('Error:', error));
 }
